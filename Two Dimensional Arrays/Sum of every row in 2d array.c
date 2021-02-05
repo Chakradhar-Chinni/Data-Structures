3a590b66ad609ea49eed83963a65e481fa6e247b
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-02-05 08:55:01 +0530

    Create Sum of every row in 2d array.c

---
+++ Two Dimensional Arrays/Sum of every row in 2d array.c
@@ -0,0 +1,46 @@
+#include<stdio.h>   //printing sum of every row in a 2d matrix
+int main()
+{
+	int i,j,r,c,k=0,sum=0;
+	scanf("%d%d",&r,&c);
+	int arr[r][c];
+	int row_sum[r*c];
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	
+	
+	printf("---------Matrix---------\n\n"); //Printing the Matrix
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d\t",arr[i][j]);
+		}
+		printf("\n");
+	}
+	
+	//Calculating sum of every row
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			sum = sum+arr[i][j];
+		}
+		row_sum[k] = sum;
+		k++;
+		sum = 0;
+	}
+	
+	//Printing Sum of every row
+	printf("\n----Sum of every Row----\n\n");
+	for(i=0;i<k;i++)
+	{
+		printf("Sum of Row[%d] : %d\n",i+1,row_sum[i]);
+	}
+}
