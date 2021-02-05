c33dda1b255b7566673ee969c2db458ea7b60bdb
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-02-05 15:43:08 +0530

    Create Sum of every column  in 2d array.c

---
+++ Two Dimensional Arrays/Sum of every column  in 2d array.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+	int r,c,i,j,sum=0,k=0;
+	scanf("%d%d",&r,&c);
+	int arr[r][c];
+	int col_sum[r*c];
+	
+	//Scanning every element
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	
+	printf("\n--Matrix--\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d\t",arr[i][j]);
+		}
+		printf("\n");
+	}
+			
+	//Sum of every column
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			sum = sum+arr[j][i];
+		}
+		col_sum[k] = sum;
+		k++;
+		sum=0;
+	}
+	
+	printf("\n\n--Column Sum--\n\n");
+	for(i=0;i<k;i++)
+	{
+		printf("Column[%d] : %d\n",i+1,col_sum[i]);
+	}
+	
+}
+
+
