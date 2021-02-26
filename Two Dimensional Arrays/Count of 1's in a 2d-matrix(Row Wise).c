97e30531be258ff280a02e93cdd289659c57a0a8
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-02-26 15:23:49 +0530

    Update Count of 1's in a 2d-matrix(Row Wise).c

--- Two Dimensional Arrays/Count of 1's in a 2d-matrix(Row Wise).c
+++ Two Dimensional Arrays/Count of 1's in a 2d-matrix(Row Wise).c
@@ -5,31 +5,31 @@ int main()
 	 scanf("%d%d",&r,&c);
 	 int arr[r][c];
 	 int rc[r];
-	 
+	
 	 for(i=0;i<r;i++)
 	 {
-	 	for(j=0;j<c;j++)
-		 	{
-		 		scanf("%d",&arr[i][j]);
-			}
+	   for(j=0;j<c;j++)
+	    {
+	      scanf("%d",&arr[i][j]);
+	    }
 	 }
 	 
 	 for(i=0;i<r;i++)
 	 {
-	 	for(j=0;j<c;j++)
-		 	{
-		 		if(arr[i][j]==1)
-		 		{
-		 			count++;
-				}
-			}
-			rc[i] = count;
-			count=0;
+	   for(j=0;j<c;j++)
+	    {
+	      if(arr[i][j]==1)
+		{
+		   count++;
+		}
+	    }
+            rc[i] = count;
+	    count=0;
 	 }
 	 
 	 printf("----Output----\n");
 	 for(i=0;i<r;i++)
 	 {
-	 		printf("Count of 1's in Row[%d]: %d\n",(i+1),rc[i]);
+	    printf("Count of 1's in Row[%d]: %d\n",(i+1),rc[i]);
 	 }
 }
