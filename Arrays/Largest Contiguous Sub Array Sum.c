2f36536803ba7879a30a22bbb836cccfb7b03992
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-06 19:54:22 +0530

    Create Largest Contiguous Sub Array Sum.c

---
+++ Arrays/Largest Contiguous Sub Array Sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int max_so_far=0;
+    int max_ending_here=0;
+    
+    int arr[8] = {-2,-3,4,-1,-2,1,5,3};
+    
+    for(int i=0;i<8;i++)
+    {
+       //for(int j=i;j<8;j++)
+       //{
+           int temp = max_ending_here+arr[i];
+           if(temp>0){
+           max_ending_here = max_ending_here+arr[i];}
+           if(max_so_far<max_ending_here)
+           {
+               max_so_far = max_ending_here;
+           }
+       //}
+    }
+    printf("%d",max_so_far);
+}
