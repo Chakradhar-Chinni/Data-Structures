a87046ca064bc1749a1483017ff580580d491b80
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-20 10:35:09 +0530

    Create Frequencies of every character in sorted order in a string.java

---
+++ Strings/Frequencies of every character in sorted order in a string.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		int[] count = new int[26];
+		int i;
+		for(i=0;i<str.length();i++)
+		{
+			count[str.charAt(i)-'a']++;
+		}
+		for(i=0;i<26;i++)
+		{
+			if(count[i]>0)
+			{
+				System.out.println((char)(i+'a') + " "+count[i]);
+			}
+		}
+	}
+}
