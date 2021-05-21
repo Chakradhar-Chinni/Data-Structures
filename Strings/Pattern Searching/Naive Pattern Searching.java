1b16e6cc17ebbf2ce6be37fc698ea2742372c005
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-21 11:43:32 +0530

    Update Naive Pattern Searching.java

--- Strings/Pattern Searching/Naive Pattern Searching.java
+++ Strings/Pattern Searching/Naive Pattern Searching.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+public class Naive_Pattern_Search {
+	static int nps(String text,String pattern)
+	{
+		int i,j;
+		for(i=0;i<=(text.length()-pattern.length());i++)
+		{
+			for(j=0;j<pattern.length();j++)
+			{
+				if(pattern.charAt(j)!=text.charAt(i+j))  //sliding through text
+				{	
+					break;
+				}
+			}
+			if(j==pattern.length()) //if entire pattern is matched
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String text = sc.nextLine();
+		String pattern = sc.nextLine();
+		System.out.println(nps(text,pattern));
+	}
+
+}
