92054416a559c031fab76797821bd18500b913bc
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-05-21 09:31:28 +0530

    Better Approach code added

--- Strings/left most repeating character.java
+++ Strings/left most repeating character.java
@@ -1,7 +1,7 @@
 //Naive Approach
 //Returning the index of the left most repeating character, if there is no repeating character return -1
 import java.util.Scanner;
-public class Main 
+public class Naive_Approach 
 {
 	static int lmc(String s)
 	{
@@ -15,6 +15,35 @@ public class Main
 				{
 					return i;
 				}
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println(lmc(s));
+	}
+}
+===================
+import java.util.Scanner;
+
+public class Better_Approach
+{
+	static int lmc(String s)
+	{
+		int[] count = new int[256]; //256 sized array to accept all ASCII values
+		int i,j;
+		for(i=0;i<s.length();i++)
+		{
+			count[s.charAt(i)]++;
+		}
+		for(i=0;i<s.length();i++)
+		{
+			if(count[s.charAt(i)]>1)
+			{
+				return i;
 			}
 		}
 		return -1;
