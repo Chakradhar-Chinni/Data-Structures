8238107a5e62101d7df547597cd8146867855ced
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-07-11 15:49:26 +0530

    Efficient Approach

--- Arrays/Maximum Consecutive 1's.java
+++ Arrays/Maximum Consecutive 1's.java
@@ -40,3 +40,37 @@ public class Main
     	System.out.println("\n"+max);
 	}
 }
+//Efficient Approach
+//TC: Linear Time O(n)
+import java.util.Scanner;
+public class Main
+{
+    static int maxconsecutive1s(int[] arr,int n)
+    {
+        int i,j,curr_count=0,max_count=0;
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+                curr_count=0;
+            else
+                curr_count++;
+                if(curr_count>max_count)
+                {
+                    max_count = curr_count;
+                }
+        }
+        return max_count;
+    }
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i] = sc.nextInt();
+		}
+		int max = maxconsecutive1s(arr,n);
+		System.out.println("\n"+max);
+	}
+}
