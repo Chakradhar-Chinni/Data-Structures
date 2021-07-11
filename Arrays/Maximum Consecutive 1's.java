bd54d081cb85231183ad32e12f9304df7bc62ebe
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-07-11 15:48:42 +0530

    Naive Approach

---
+++ Arrays/Maximum Consecutive 1's.java
@@ -0,0 +1,42 @@
+//Naive Approach
+//TC: O(n^2)
+import java.util.Scanner; 
+public class Main
+{
+    static int max1s(int[] arr,int n)
+    {
+        int i,j,curr_count=0,max_count=0,flag=0;
+        for(i=0;i<n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                if(arr[j]==1)
+                {
+                    curr_count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if(curr_count > max_count)
+            {
+                max_count = curr_count;
+            }
+            curr_count=0;
+        }
+        return max_count;
+    }
+	public static void main(String[] args) 
+	{
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int[] arr = new int[n];
+    	for(int i=0;i<n;i++)
+    	{
+    	    arr[i] = sc.nextInt();
+    	}
+    	int max = max1s(arr,n);
+    	System.out.println("\n"+max);
+	}
+}
