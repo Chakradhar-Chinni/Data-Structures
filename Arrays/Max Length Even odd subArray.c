eaea7ebfa42b3929626c122c7481a8372b2b56f1
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-07-13 19:56:30 +0530

    Naive Approach

---
+++ Arrays/Max Length Even odd subArray.c
@@ -0,0 +1,33 @@
+//Naive Approach
+//TC: O(n^2)
+#include<stdio.h> //Maximum Length Even odd Sub array                
+int max(int n,int arr[])
+{
+	int curr,i,j,res=1;
+	for(i=0;i<n;i++) 
+	{
+		curr=1;
+		for(j=i+1;j<n;j++)
+		{ 
+			if( (arr[j]%2==0 && arr[j-1]%2!=0) || (arr[j]%2!=0 && arr[j-1]%2==0) ) 
+				curr++;
+			else
+				break;
+		}
+		if(curr>res)
+			res = curr; 
+	}
+	return res;
+}
+int main()
+{
+	int i,n;
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<n;i++)
+	{
+	  scanf("%d",&arr[i]);
+	}
+	int res = max(n,arr);
+	printf("\n%d",res);
+}
