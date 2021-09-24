33faca4cf8a24d2a98cb7bde66404d06fc96c1f1
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-09-24 17:02:50 +0530

    O(n) Approach

--- Arrays/Sort arrays of 0 and 1.java
+++ Arrays/Sort arrays of 0 and 1.java
@@ -34,3 +34,42 @@ class Sort
 		}
 	}
 }
+//O(n)
+import java.util.*;
+class Sort
+{
+	public static void main(String[] args)
+	{                                          
+		//int arr[] = {1,2,3,4,5,6,7,8,9,10};
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i,count=0;
+		int[] arr = new int[n];
+		for(i=0;i<n;i++) {
+			arr[i] = sc.nextInt();	}
+		int left = 0;
+		int right = n-1;
+		while(left<right)
+		{
+			if(arr[i]==0)
+			{
+				left++;
+			}
+			if(arr[i]==1)
+			{
+				right--;
+			}
+			if(left<right)
+			{
+				arr[left]=0;
+				arr[right]=1;
+				left++;
+				right--;
+			}
+		}
+		for(i=0;i<n;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
