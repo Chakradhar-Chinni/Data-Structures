050bea4a38a6cf5b09a2ccba8b4357550b13abeb
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-07 21:37:45 +0530

    Efficient Approach

--- Hashing/Count distinct elements.java
+++ Hashing/Count distinct elements.java
@@ -35,3 +35,31 @@ class Main
         System.out.println(result);
     }
 }
+
+//Efficient Approach
+import java.util.*;
+class Main
+{
+    public static int function(int[] arr,int n)
+    {
+        int i;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(i=0;i<n;i++)
+        {
+            set.add(arr[i]);
+        }
+        return set.size();
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int result = function(arr,n);
+        System.out.println(result);
+    }
+}
