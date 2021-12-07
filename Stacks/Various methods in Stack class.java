10703931ff46486f4bf96cc93c26d82145c41041
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-07 13:53:58 +0530

    search()

--- Stacks/Various methods in Stack class.java
+++ Stacks/Various methods in Stack class.java
@@ -40,3 +40,46 @@ Output:
 [10, 20]
 [10]
 [10]
+
+//search()
+import java.util.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+      Stack<Integer> s = new Stack<Integer>();   
+      s.push(10);
+      s.push(20);
+      s.push(30);
+      s.push(40);
+      s.push(50);
+      s.push(60);
+      s.push(70);
+      System.out.println(s);
+      
+      System.out.println("Search Method");
+      System.out.println(s.search(70));
+      System.out.println(s.search(30));
+      
+      //to get actual position see the below code - useful for coding
+      System.out.println("Acutal Index method");
+      System.out.println(s.size()-s.search(70));
+      System.out.println(s.size()-s.search(30));
+    }
+}
+
+/*
+searches the element from top of stack using 1-based index and returns first occurence
+returns -1 if element is not found
+
+*/
+Output:
+[10, 20, 30, 40, 50, 60, 70]
+Search Method
+1
+5
+Acutal Index method
+6
+2
+
