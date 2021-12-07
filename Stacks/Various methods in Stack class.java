88b7a4e26e142b1f5d4aa09d745192bdedf74aa7
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-07 13:41:31 +0530

    Update Various methods in Stack class.java

--- Stacks/Various methods in Stack class.java
+++ Stacks/Various methods in Stack class.java
@@ -14,3 +14,29 @@ class Main
 
 Output:
 [10, 20]
+
+//pop() removes top element
+//peek() returns top element
+import java.util.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+      Stack<Integer> s = new Stack<Integer>();   
+      s.push(10);
+      s.push(20);
+      System.out.println(s);
+      
+      s.pop(); //removes top element
+      System.out.println(s);
+      
+      s.peek();   //displays top element
+      System.out.println(s);
+    }
+}
+
+Output:
+[10, 20]
+[10]
+[10]
