1ef4b0e9e6b64d208ebb2751c56c5b3931d1e93d
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-07 13:28:43 +0530

    for pushing elements to stack

---
+++ Stacks/Various methods in Stack class.java
@@ -0,0 +1,16 @@
+//for pushing elements to stack
+import java.util.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+      Stack<Integer> s = new Stack<Integer>();   
+      s.push(10);
+      s.push(20);
+      System.out.print(s);
+    }
+}
+
+Output:
+[10, 20]
