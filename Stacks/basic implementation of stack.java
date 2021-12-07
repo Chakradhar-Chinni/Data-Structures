1109e86b7af07ff3d63e495664f40bcaf56abdf6
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-07 13:21:08 +0530

    Create basic implementation of stack.java

---
+++ Stacks/basic implementation of stack.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Stack<Integer> s = new Stack<Integer>();
+        //Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        System.out.print(s);
+        
+    }
+}
+/*
+Output:
+[10, 20]
+*/
