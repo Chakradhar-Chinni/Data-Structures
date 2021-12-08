7b066b08401fe34d86dbe627be2ecf5779841f36
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-08 14:18:21 +0530

    Efficient Approach

--- Hashing/Intersection of two arrays.java
+++ Hashing/Intersection of two arrays.java
@@ -41,3 +41,44 @@ class Main
         System.out.println(res);
     }
 }
+
+//Efficient Approach
+import java.util.*;
+class Main
+{
+    public static int function(int[] arr1,int[] arr2)
+    {
+        int i,count=0;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(i=0;i<arr1.length;i++)
+        {
+            set.add(arr1[i]);
+        }
+        for(i=0;i<arr2.length;i++)
+        {
+            if(set.contains(arr2[i]))
+            {
+                set.remove(arr2[i]);
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args)
+    {
+         Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        
+        int[] arr1 = new int[n1];
+        int[] arr2 = new int[n2];
+        
+        for(int i=0;i<n1;i++) {
+            arr1[i] = sc.nextInt(); }
+        for(int i=0;i<n2;i++) {
+            arr2[i] = sc.nextInt(); }
+            
+        int res = function(arr1,arr2);
+        System.out.println(res);
+    }
+}
