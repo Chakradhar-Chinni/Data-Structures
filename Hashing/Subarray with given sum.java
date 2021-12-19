13126aa19119a39239942a0dceec65d4107e94d1
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-19 23:14:51 +0530

    Update Subarray with given sum.java

--- Hashing/Subarray with given sum.java
+++ Hashing/Subarray with given sum.java
@@ -1,3 +1,34 @@
+//Naive Approach
+import java.util.*;
+class Main
+{
+    public static boolean func(int n,int[] arr,int sum)
+    {
+        int j,i,t_sum;
+        for(i=0;i<n-1;i++)
+        {
+            t_sum = arr[i];
+            for(j=i+1;j<n;j++)
+            {
+                t_sum = t_sum+arr[j];
+                if(t_sum==sum)
+                    return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();}
+        boolean res =func(n,arr,sum);
+        System.out.println(res);
+    }
+}
 
 Test Cases
 Input:
